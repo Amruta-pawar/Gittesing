9741ae2da69d111c7aa5cce9c037b66b52b22b34
supriya <supriyabhakare22@gmail.com>
2019-11-20 12:13:03 +0530

    file to git

---
+++ GitHub/src/A/wel.java
@@ -0,0 +1,8 @@
+package A;
+
+public class wel {
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
---
+++ GitHub/src/X/jolly.java
@@ -0,0 +1,8 @@
+package X;
+
+public class jolly {
+	public static void main(String[] args) {
+		System.out.println("jolly");
+	}
+
+}
